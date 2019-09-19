93d8e812996f3b868c4ec976a7d2478f948735e7
Edward997 <edward.navarrete7@gmail.com>
2019-09-19 10:21:42 -0500

    nodos 2

--- Estructura nodos.cpp
+++ Estructura nodos.cpp
@@ -6,72 +6,39 @@ using namespace std;
 struct nodo{
 	int valor;
 	nodo* ptr;
-	};
+	
+	nodo(int aux=0){
+        valor=aux;
+        ptr=NULL;
+    }
+	
+	
+	void nuevo(int aux){
+        nodo *actual=ptr;
+        nodo *newnodo=new nodo(aux);
+        if(actual!=NULL) while(actual->ptr!=NULL) actual=actual->ptr;
+        if(actual==NULL) ptr=newnodo;
+        else actual->ptr=newnodo;
+    }
+    void mostrar(){
+        cout<<valor<<endl;
+        nodo *actual=ptr;
+        while(actual!=NULL){
+            cout<<actual->valor<<endl;
+            actual=actual->ptr;
+        }
+    }
+    };
+
+nodo l1(3);
 
 int main(int argc, char** argv){
 	
-	nodo* inicio;
-	nodo* aux;
-	
-	inicio=NULL;
-	aux= new nodo();
-	
-	aux->valor=5;
-	aux->ptr=NULL;
-	inicio=aux;
-	
-	aux= new nodo();
-	aux->valor=6;
-	aux->ptr=NULL;
-	inicio -> ptr =aux;
-	
-	aux= new nodo();
-	aux->valor=4;
-	aux->ptr=NULL;
-	inicio ->ptr ->ptr =aux;	
-	
-	aux= new nodo();
-	aux->valor=1;
-	aux->ptr=NULL;
-	inicio ->ptr ->ptr ->ptr=aux;
-	
-	aux= new nodo();
-	aux->valor=9;
-	aux->ptr=NULL;
-	inicio ->ptr ->ptr ->ptr ->ptr=aux;
-	
-	
-	cout<< inicio->valor <<endl;
-	cout<< inicio->ptr->valor <<endl;
-	cout<< inicio->ptr->ptr->valor <<endl;
-	cout<< inicio->ptr->ptr->ptr->valor <<endl;
-	cout<< inicio->ptr->ptr->ptr->ptr->valor <<endl;
-	
-	
-
-	
-	struct nodo a,b,c,d,e;
-	a.valor=4;
-	a.ptr=NULL;
-	b.valor=5;
-	b.ptr=NULL;
-	c.valor=8;
-	c.ptr=NULL;
-	d.valor=7;
-	d.ptr=NULL;
-	e.valor=9;
-	e.ptr=NULL;
-	 
-	a.ptr=&b;
-	b.ptr=&c;
-	c.ptr=&d;
-	d.ptr=&e;
-	
-	cout<<a.valor <<endl;
-	cout<<a.ptr->valor<<endl;
-	cout<<a.ptr->ptr->valor<<endl;
-	cout<<a.ptr->ptr->ptr->valor<<endl;
-	cout<<a.ptr->ptr->ptr->ptr->valor;
+	l1.nuevo(2);
+    l1.nuevo(9);
+    l1.nuevo(3);
+    l1.nuevo(1);
+    l1.mostrar();
 
 	return 0;
 }
